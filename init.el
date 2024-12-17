4e1c933f50e52af0c9a3fc33cf2ae0a1fa5729cb
Claudemiro A F Neto <16993+dimiro1@users.noreply.github.com>
2024-12-17 16:06:37 +0100

    Back to emacs after so many years :)

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 ;;; .emacs.d --- My Emacs Config
 ;;; Commentary:
 
-;; Copyright (C) 2015,2016 Claudemiro Alves Feitosa Neto <dimiro1@gmail.com>
+;; Copyright (C) 2015,2016,2024 Claudemiro Alves Feitosa Neto <dimiro1@gmail.com>
 ;; 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -16,12 +16,23 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-;;; Code:
-
-;; The first thing to do
+;; UTF-8
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
+
+;; Initial Window size
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; No splash screen
+(setq inhibit-startup-screen t)
+
+;; Font face
+(set-frame-font "Comic Code-13")
+
+;; Update Path
+(add-to-list 'exec-path (concat (getenv "HOME") "/go/bin"))
+(add-to-list 'exec-path (concat (getenv "HOME") ".cargo/bin"))
 
 ;; Load custom first
 (setq custom-file "~/.emacs.d/custom.el")
@@ -33,161 +44,103 @@
 (scroll-bar-mode -1)
 (show-paren-mode)
 
-;; No splash screen
-(setq inhibit-startup-screen t)
-
-;; No Bold
-(set-face-bold 'bold nil)
-
-;; Common LISP
-
-(eval-when-compile (require 'cl))
-
 ;; Packages
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
-(package-initialize)
+(setq package-enable-at-startup nil)
 
-;; Install packages
-(defvar my-packages '(
-		   web-mode
-		   paredit
-		   linum-relative
-		   helm
-		   hl-todo
-		   company
-		   company-go
-		   majapahit-theme
-		   dracula-theme
-		   gotham-theme
-		   color-theme-modern
-		   ample-zen-theme
-		   monokai-theme
-		   railscasts-theme
-		   atom-dark-theme
-		   magit
-		   fiplr
-		   atom-one-dark-theme
-		   markdown-mode+
-		   jbeans-theme
-		   darcula-theme
-		   go-mode
-		   go-guru
-		   go-rename
-		   flycheck
-		   rainbow-delimiters
-		   exec-path-from-shell)
-  "Packages to install.")
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
+(use-package company :straight t :ensure t)
+(use-package magit :straight t :ensure t)
+(use-package fiplr :straight t :ensure t)
+(use-package gotham-theme :straight t :ensure t)
+(use-package spacemacs-theme :straight t :ensure t)
+(use-package gruvbox-theme :straight t :ensure t)
+(use-package copilot-chat
+  :straight (:host github :repo "chep/copilot-chat.el" :files ("*.el"))
+  :after (request org markdown-mode shell-maker))
 
-(loop for pkg in my-packages
-      unless (package-installed-p pkg) do (package-install pkg))
+;; Copilot
 
-;; env
-(when (memq window-system '(mac ns x))
-  (setq-default exec-path-from-shell-check-startup-files nil)
-  (setq-default exec-path-from-shell-variables '("PATH" "GOPATH" "GOROOT"))
-  (exec-path-from-shell-initialize))
+; Adds a hook to automatically insert a commit message using GitHub Copilot Chat when preparing a Git commit.
+(add-hook 'git-commit-setup-hook 'copilot-chat-insert-commit-message)
 
-;; minor modes
-;; Linum relative
-(require 'linum-relative)
-(linum-relative-global-mode)
-(setq linum-relative-current-symbol "")
-(setq linum-relative-format "%3s ")
+;; Theme
+(load-theme 'spacemacs-dark)
 
-;; Helm
+;; Fiplr
+(setq fiplr-root-markers (append fiplr-root-markers '("go.mod")))
 
-(require 'helm-config)
-(global-set-key (kbd "M-x") 'helm-M-x)
+(let ((dirs (cadr (assoc 'directories fiplr-ignored-globs))))
+  (setcdr (assoc 'directories fiplr-ignored-globs) (list (append dirs '(".idea")))))
 
+;; Display line numbers relative
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode t)
 
-;; Ido
+;; treesit
+
+(require 'treesit)
+
+(setq treesit-language-source-alist
+ '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+   (css "https://github.com/tree-sitter/tree-sitter-css")
+   (go "https://github.com/tree-sitter/tree-sitter-go")
+   (gomod "https://github.com/camdencheek/tree-sitter-go-mod")
+   (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+   (json "https://github.com/tree-sitter/tree-sitter-json")
+   (make "https://github.com/alemuller/tree-sitter-make")
+   (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+   (python "https://github.com/tree-sitter/tree-sitter-python")
+   (toml "https://github.com/tree-sitter/tree-sitter-toml")
+   (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+   (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
+
+;; Golang
+(add-to-list 'auto-mode-alist '("\\.go\\'" . go-ts-mode))
+(add-hook 'go-ts-mode-hook 'eglot-ensure)
+(add-hook 'go-ts-mode-hook 'company-mode)
+
+;; elisp
+(add-hook 'emacs-lisp-mode-hook 'company-mode)
+
+;; copilot
+(setq copilot-chat-frontend 'markdown)
+
+;; IDO
 (setq-default ido-use-filename-at-point 'guess)
 (setq-default ido-create-new-buffer 'always)
 (setq-default ido-enable-flex-matching t)
 (setq-default ido-everywhere t)
 (ido-mode t)
 
-;; linum
-(global-linum-mode t)
-(global-hl-todo-mode t)
-(column-number-mode)
-
-;; Flycheck
-(add-hook 'after-init-hook #'global-flycheck-mode)
-
-;; Fiplr
-(setq-default fiplr-root-markers '(".git"
-								   "project.clj"
-								   "build.gradle"
-								   "Gopkg.toml"))
-
-(setq-default fiplr-ignored-globs '((directories (".svn" ".git" ".hg" "CVS" "build" "target"))
-									(files ("*.pyc" "*.pyo" "*.exe" "*.dll" "*.obj""*.o"
-											"*.a" "*.lib" "*.so" "*.dylib" "*.ncb" "*.sdf"
-											"*.suo" "*.pdb" "*.idb" ".DS_Store" "*.class"
-											"*.psd" "*.db" "*.jpg" "*.jpeg" "*.png" "*.gif"
-											"*.ttf" "*.tga" "*.dds" "*.ico" "*.eot" "*.pdf"
-											"*.swf" "*.jar" "*.zip"))))
-
-(windmove-default-keybindings)
-(electric-pair-mode)
-
-;; Hooks
-(add-hook 'emacs-lisp-mode-hook #'paredit-mode)
-(add-hook 'emacs-lisp-mode-hook #'rainbow-delimiters-mode)
-(add-hook 'before-save-hook #'gofmt-before-save)
-
-;; Auto complete
-(add-hook 'after-init-hook 'global-company-mode)
-
-(add-hook 'go-mode-hook (lambda ()
-                          (set (make-local-variable 'company-backends) '(company-go))
-                          (company-mode)
-						  (go-guru-hl-identifier-mode)))
-
-
-;; Custom Keybindings
-(global-set-key (kbd "C-c l") 'goto-line)
-(global-set-key (kbd "C-c p") 'fiplr-find-file)
-(global-set-key (kbd "C-c s") 'magit-status)
-(global-set-key (kbd "C-c c") 'magit-push-current)
-
-(global-set-key (kbd "C-M-<left>") 'shrink-window-horizontally)
-(global-set-key (kbd "C-M-<right>") 'enlarge-window-horizontally)
-(global-set-key (kbd "C-M-<down>") 'shrink-window)
-(global-set-key (kbd "C-M-<up>") 'enlarge-window)
-
-(global-set-key (kbd "M-TAB") #'company-complete)
-
-;; (global-set-key [s-left] 'windmove-left)
-;; (global-set-key [s-right] 'windmove-right)
-;; (global-set-key [s-up] 'windmove-up)
-;; (global-set-key [s-down] 'windmove-down)
-
-(setq-default line-spacing 2)
-(setq-default default-tab-width 4)
-
-;; Color Theme
-
-(load-theme 'dracula)
 
 ;; Backups
 (setq backup-directory-alist `(("." . "~/.emacs-saves")))
 (setq backup-by-copying t)
 (setq delete-old-versions t
   kept-new-versions 6
-  kept-old-versions 2
+  kept-old-versionsp 2
   version-control t)
 
-;; Custom Editor
-(set-frame-font "Inconsolata-14")
-;;(set-frame-font "Liberation Mono-11")
+;; Custom keybindgs
 
-(provide 'init)
-;;; init.el ends here
+(global-set-key (kbd "C-c p") 'fiplr-find-file)
+(global-set-key (kbd "C-M-<left>") 'shrink-window-horizontally)
+(global-set-key (kbd "C-M-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "C-M-<down>") 'shrink-window)
+(global-set-key (kbd "C-M-<up>") 'enlarge-window)
