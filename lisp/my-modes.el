d96a471522ac6bea39646b32a16e77b323aa8849
Claudemiro <dimiro1@gmail.com>
2024-12-28 22:41:56 +0100

    Ensure org mode recognizes go files.

--- lisp/my-modes.el
+++ lisp/my-modes.el
@@ -1,4 +1,12 @@
 ;; Install Markdown mode
 (use-package markdown-mode)
 
+
+(use-package emacs
+  :config
+  ;; Ensure Org Babel recognizes Go code
+  (with-eval-after-load 'org
+  (add-to-list 'org-src-lang-modes '("go" . go-ts))))
+
+
 (provide 'my-modes)
