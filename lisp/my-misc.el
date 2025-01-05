8fc3419f47745a404cddaaeec0d4da55b3641fd2
claudemiro <dimiro1@gmail.com>
2025-01-05 15:32:32 +0100

    Loads the package xid on demand.

--- lisp/my-misc.el
+++ lisp/my-misc.el
@@ -4,6 +4,7 @@
 ;; Generates globally unique IDs (xid github.com/rs/xid)
 ;; Usage: C-c x i d → cto6h99m65j2ggqeebc0
 (use-package xid
+  :demand t
   :straight ( :type git :host github :repo "dimiro1/xid.el" )
   :bind ( "C-c x i d" . xid-insert ))
 
