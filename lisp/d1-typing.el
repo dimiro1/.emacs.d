5675bb6a218621c64feb99816fa08a2b3856b4cd
Claudemiro A F Neto <claudemiro@spaceshelf.co>
2025-08-16 17:42:04 +0200

    Add yasnippets for snippets.

--- lisp/d1-typing.el
+++ lisp/d1-typing.el
@@ -1,8 +1,10 @@
-;;; d1-typing.el --- Configuration for speed-type package -*- lexical-binding: t; -*-
+;;; d1-typing.el --- Configuration for typing enhancement packages -*- lexical-binding: t; -*-
 
 ;;; Commentary:
-;; This file configures the speed-type package from:
-;; https://codeberg.org/rafalraczka/speed-type/
+;; This file configures typing enhancement packages:
+;; - speed-type: typing practice (https://codeberg.org/rafalraczka/speed-type/)
+;; - yasnippet: template system for Emacs (https://github.com/joaotavora/yasnippet)
+;; - yasnippet-snippets: collection of snippets (https://github.com/AndreaCrotti/yasnippet-snippets)
 
 ;;; Code:
 
@@ -12,6 +14,18 @@
   ;; Optional customizations
   (speed-type-min-chars 200)
   (speed-type-max-chars 450))
+
+(use-package yasnippet
+  :ensure t
+  :diminish yas-minor-mode
+  :hook ((prog-mode . yas-minor-mode)
+         (text-mode . yas-minor-mode))
+  :config
+  (yas-reload-all))
+
+(use-package yasnippet-snippets
+  :ensure t
+  :after yasnippet)
 
 (provide 'd1-typing)
 ;;; d1-typing.el ends here
